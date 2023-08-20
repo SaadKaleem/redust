b4f8ed93ebfbe386b3c2d821d9113d0d896f3502
Saad Kaleem <skaleem@pandata.qa>
2023-08-20 01:03:17 +0000

    ✅ Update PING and ECHO Tests

--- tests/test_cmd_integration.rs
+++ tests/test_cmd_integration.rs
@@ -11,31 +11,67 @@ fn cnxn() -> Connection {
 }
 
 #[rstest]
-fn test_ping(mut cnxn: Connection) -> RedisResult<()> {
-    let ping_response: String = redis::cmd("PING").query(&mut cnxn)?;
+// No Arguments
+#[case("PING", None, None)]
+// Single Argument
+#[case("PING", Some("Hello, World!".to_string()), None)]
+// Extra Argument
+#[case("PING", Some("Hello, World!".to_string()), Some("Hello, World!".to_string()))]
+fn test_ping(
+    #[case] command: &str,
+    #[case] message: Option<String>,
+    #[case] extra_arg: Option<String>,
+    mut cnxn: Connection,
+) -> RedisResult<()> {
+    let response: Result<String, redis::RedisError> = redis::cmd(command)
+        .arg(&message)
+        .arg(&extra_arg)
+        .query(&mut cnxn);
 
-    assert_eq!(ping_response, "PONG");
+    if extra_arg.is_some() {
+        assert_eq!(
+            response.err().unwrap().to_string(),
+            "wrong: number of arguments for 'ping' command".to_string()
+        );
+    } else if message.is_some() {
+        assert_eq!(
+            response.unwrap(),
+            format!("{}{}{}", "\"", message.unwrap(), "\"")
+        );
+    } else {
+        assert_eq!(response.unwrap(), format!("{}{}{}", "\"", "PONG", "\""));
+    }
+
     Ok(())
 }
 
 #[rstest]
-#[case("ECHO", Some("Hello, World!".to_string()))]
+#[case("ECHO", Some("Hello, World!".to_string()), None)]
 // Missing Argument
-#[case("ECHO", None)]
+#[case("ECHO", None, None)]
+// Extra Argument
+#[case("ECHO", Some("Hello, World!".to_string()), Some("Hello, World!".to_string()))]
 fn test_echo(
     #[case] command: &str,
     #[case] message: Option<String>,
+    #[case] extra_arg: Option<String>,
     mut cnxn: Connection,
 ) -> RedisResult<()> {
-    let response: Result<String, redis::RedisError> =
-        redis::cmd(command).arg(&message).query(&mut cnxn);
+    let response: Result<String, redis::RedisError> = redis::cmd(command)
+        .arg(&message)
+        .arg(&extra_arg)
+        .query(&mut cnxn);
 
-    match message {
-        Some(message) => assert_eq!(response.unwrap(), message),
-        None => assert_eq!(
+    if extra_arg.is_some() || message.is_none() {
+        assert_eq!(
             response.err().unwrap().to_string(),
-            "ERR:: Missing Argument".to_string()
-        ),
+            "An error was signalled by the server - ResponseError: wrong number of arguments for 'echo' command".to_string()
+        );
+    } else {
+        assert_eq!(
+            response.unwrap(),
+            format!("{}{}{}", "\"", message.unwrap(), "\"")
+        );
     }
 
     Ok(())
