8f913453fa171796ce1f58ca066236a6c1f5ecf6
Saad Kaleem <skaleem@pandata.qa>
2023-08-20 01:04:05 +0000

    🐛 Update PING
    * Handles the optional message command now

--- src/cmd/ping.rs
+++ src/cmd/ping.rs
@@ -2,27 +2,42 @@ use crate::cmd::ParseError;
 use crate::{Connection, RESPType};
 
 #[derive(Debug, Default)]
-pub struct Ping {}
+pub struct Ping {
+    msg: Option<String>,
+}
 
 impl Ping {
     /// Create a new `Ping` command
-    pub fn new() -> Ping {
-        Ping {}
+    pub fn new(msg: Option<String>) -> Ping {
+        Ping { msg }
     }
 
     pub fn parse(cmd_strings: Vec<String>) -> Result<Ping, ParseError> {
-        if cmd_strings.len() > 1 {
-            Err(ParseError::ExtraCmdArg(
-                "ERR: Found more than one arg for PING".to_string(),
-            ))
+        if cmd_strings.len() > 2 {
+            return Err(ParseError::ExtraCmdArg(
+                "wrong number of arguments for 'ping' command".to_string(),
+            ));
         } else {
-            Ok(Ping::new())
+            // Get the value at index 1, and pass the argument so it can be cloned in a different closure.
+            let msg = cmd_strings.get(1).map(|s| s.clone());
+
+            match msg {
+                Some(val) => {
+                    return Ok(Ping::new(Some(val)));
+                }
+                None => {
+                    return Ok(Ping::new(None));
+                }
+            }
         }
     }
 
     /// Execute the "Ping" command and return PONG
     pub async fn execute(self, cnxn: &mut Connection) -> Result<(), Box<dyn std::error::Error>> {
-        let resp = RESPType::SimpleString("PONG".to_string());
+        let resp = match self.msg {
+            Some(msg) => RESPType::SimpleString(format!("{}{}{}", "\"", msg, "\"")),
+            None => RESPType::SimpleString("\"PONG\"".to_string()),
+        };
 
         // Write the response back to the client
         let _ = cnxn.write_frame(&resp).await;
